295b1226066bca188b4fd420968b36f214737887
Squidmond <blightr@msn.com>
2021-09-19 00:36:10 -0500

    Post Lesson 309

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,23 +1,25 @@
 console.log(`*** Script Starting ***`);
-const button = document.getElementById("clickMeBtn");
-console.dir(button);
-// Best way to add event
-// button.addEventListener("click", (btn) => {
-//     console.log("Button was clicked");
-// });
+const buttons = document.querySelectorAll("button");
+console.dir(buttons);
 
-// Another way
-// button.onclick = function (ptrEvnt) {
-//     // Could also be a function expression or arrow function
-//     console.log('Pointer Event', ptrEvnt);
-//     console.log(`ptrEvent.srcElement: `, ptrEvnt.srcElement);
-//     console.log(`ptrEvnt.srcElement.id: `, ptrEvnt.srcElement.id);
-// }
-// Yes another way
-const btnClickHandler = (ptrEvnt) => {
-    console.log('Pointer Event', ptrEvnt);
-    console.log(`ptrEvent.srcElement: `, ptrEvnt.srcElement);
-    console.log(`ptrEvnt.srcElement.id: `, ptrEvnt.srcElement.id);
+const btnClickHandler = (event) => {
+    console.log('event', event);
+    console.log(`event.target: `, event.target);
+    console.log(`event.target.id: `, event.target.id);
+    // Only allow one click
+    //event.target.disabled = true;
 };
-button.onclick = btnClickHandler;
+const btnEnterHandler = (event) => {
+    console.log(`You entered button: ${event.target.id} area`);
+};
+// Add cllick events for all buttons
+buttons.forEach((btn, idx) => {
+    btn.addEventListener("click", btnClickHandler);
+    btn.addEventListener("mouseenter", btnEnterHandler);
+});
+// Code for infinite scroll data load
+window.addEventListener('scroll', event => {
+    console.log(event);
+});
+
 console.log(`*** End Of Line ***`);
